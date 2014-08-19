f3a00de1daebb29e6c633ad352bba2089abf885c
ohall <ohall@grassroots-tech.com>
2014-08-19 15:11:28 -0400

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('RicochetApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
