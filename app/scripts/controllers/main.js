1721df38ea19673b505b2f175a508ffeb74cfe01
ohall <ohall@grassroots-tech.com>
2014-08-26 17:56:48 -0400

    add an obstacle and to rudimentary collision detection

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,50 +3,39 @@
 angular.module('RicochetApp')
   .controller('MainCtrl', function ($scope, $interval) {
         var BALL_RADIUS = 5,
-            UPDATE_INTERVAL = 1000/60,
+            UPDATE_INTERVAL = 1000 / 60,
             bounceFactor = 0.1;
 
 
-        $scope.init = function(){
-            $scope.canvas = document.getElementById( 'gameCanvas' );
-            $scope.canvas.width =  350;
+        $scope.init = function () {
+            $scope.canvas = document.getElementById('gameCanvas');
+            $scope.canvas.width = 350;
             $scope.canvas.height = 700;
             $scope.gamemode = 'game';
             $scope.running = false;
 
             $scope.context = $scope.canvas.getContext('2d');
             var attrs = {
-                color:'blue',
-                name:'hero',
-                x:$scope.canvas.width/2,
-                y:$scope.canvas.height/2,
-                radius:BALL_RADIUS,
-                vx:300,
-                vy:300,
-                dx:10,
-                dy:10,
-                context:$scope.context
+                color: 'blue',
+                name: 'hero',
+                x: $scope.canvas.width / 2,
+                y: $scope.canvas.height / 2,
+                radius: BALL_RADIUS,
+                vx: 300,
+                vy: 300,
+                dx: 10,
+                dy: 10,
+                context: $scope.context
             };
             $scope.ball = new Ball(attrs);
+
+
+            $scope.tri = new Tri(attrs);
+
         };
 
-        $scope.drawFunc = function(ctx) {
-            if(ctx){
-                // Here, we'll first begin drawing the path and then use the arc() function to draw the circle. The arc function accepts 6 parameters, x position, y position, radius, start angle, end angle and a boolean for anti-clockwise direction.
-                ctx.beginPath();
-                ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-                //createRadialGradient(x0, y0, r0, x1, y1, r1);
-                var radialGradient = ctx.createRadialGradient(this.x+2, this.y+2, BALL_RADIUS, this.x+2, this.y+2, 1);
-                radialGradient.addColorStop(1,'#fff' );
-                radialGradient.addColorStop(0, this.color);
-                ctx.fillStyle = radialGradient;
-                ctx.fill();
-                ctx.closePath();
-            }
-        };
-
-        $scope.down = function(event){
-            if(!$scope.running) {
+        $scope.down = function (event) {
+            if (!$scope.running) {
                 $scope.context.beginPath();
                 $scope.context.moveTo($scope.ball.x, $scope.ball.y);
                 $scope.context.lineTo(event.offsetX, event.offsetY);
@@ -54,64 +43,72 @@ angular.module('RicochetApp')
             }
         };
 
-        $scope.up = function(event){
-            if(!$scope.running) {
+        $scope.up = function (event) {
+            if (!$scope.running) {
                 $scope.line = null;
-                aimAtTarget($scope.ball,event.offsetX,event.offsetY);
+                aimAtTarget($scope.ball, event.offsetX, event.offsetY);
                 $scope.start();
             }
         };
 
-        $scope.win = function(){
+        $scope.win = function () {
             $scope.stop();
             $scope.gamemode = 'win';
-            $interval( $scope.reset, 1000, 1);
+            $interval($scope.reset, 1000, 1);
         };
 
-        $scope.reset = function(){
+        $scope.reset = function () {
             $scope.init();
         };
 
-        var aimAtTarget = function(ball,targetx,targety){
-            ball.target = {x:targetx,y:targety};
-            ball.dx = (targetx - ball.x)*( bounceFactor);
-            ball.dy = (targety - ball.y)*(-bounceFactor);
+
+        var aimAtTarget = function (ball, targetx, targety) {
+            ball.target = {x: targetx, y: targety};
+            ball.dx = (targetx - ball.x) * ( bounceFactor);
+            ball.dy = (targety - ball.y) * (-bounceFactor);
         };
 
-        var clearCanvas = function(ctx) {
-            if(ctx){ ctx.clearRect(0, 0, $scope.canvas.width, $scope.canvas.height); }
+        var clearCanvas = function (ctx) {
+            if (ctx) {
+                ctx.clearRect(0, 0, $scope.canvas.width, $scope.canvas.height);
+            }
         };
 
-        var update = function() {
+        var update = function () {
             clearCanvas($scope.context);
-            if($scope.ball){
-                var contact = wallContact($scope.ball,$scope.canvas );
+            if ($scope.ball) {
+                var contact = collision($scope.ball, $scope.canvas, $scope.tri);
 
-                if(contact.top){
+                if (contact.exit) {
                     $scope.win();
                     return;
                 }
 
-                if( contact.left || contact.right ){
+                if (contact.left || contact.right) {
                     $scope.ball.vx *= -bounceFactor;
-                    $scope.ball.dx=-$scope.ball.dx;
+                    $scope.ball.dx = -$scope.ball.dx;
                 }
-                if( contact.bottom ){
+                if (contact.bottom) {
                     $scope.ball.vy *= -bounceFactor;
-                    $scope.ball.dy=-$scope.ball.dy;
+                    $scope.ball.dy = -$scope.ball.dy;
                 }
-                $scope.ball.x+=$scope.ball.dx;
-                $scope.ball.y-=$scope.ball.dy;
+                $scope.ball.x += $scope.ball.dx;
+                $scope.ball.y -= $scope.ball.dy;
                 $scope.ball.drawit();
             }
+
+            if ($scope.tri) {
+                $scope.tri.drawit();
+            }
+
         };
 
-        $scope.stop = function(){
-            $interval.cancel( $scope.updates );
+        $scope.stop = function () {
+            $interval.cancel($scope.updates);
             $scope.running = false;
         };
 
-        $scope.start = function(){
+        $scope.start = function () {
             $scope.updates = $interval(update, UPDATE_INTERVAL);
             $scope.running = true;
         };
@@ -119,28 +116,39 @@ angular.module('RicochetApp')
         /**
          * Utils
          */
-        var getBallatEdgeCoord = function(ballRadius,edgeLoc){
-            return( edgeLoc === 0 )?ballRadius:(edgeLoc - ballRadius);
+        var getBallatEdgeCoord = function (ballRadius, edgeLoc) {
+            return( edgeLoc === 0 ) ? ballRadius : (edgeLoc - ballRadius);
         };
 
-        var wallContact = function(ball, canvas){
+        var collision = function (ball, canvas, tri) {
             var contact = {
-                top:false,
-                right:false,
-                bottom:false,
-                left:false
+                top: false,
+                right: false,
+                bottom: false,
+                left: false,
+                exit:false
             };
-            if( ball.y <= getBallatEdgeCoord(BALL_RADIUS, 0 ) ){
-                contact.top = true;
+            if (ball.y <= getBallatEdgeCoord(BALL_RADIUS, 0)) {
+                contact.exit = true;
             }
-            if( ball.y >= getBallatEdgeCoord(BALL_RADIUS,canvas.height)){
+            if (ball.y >= getBallatEdgeCoord(BALL_RADIUS, canvas.height)) {
                 contact.bottom = true;
             }
-            if( ball.x <= getBallatEdgeCoord(BALL_RADIUS,0)){
+            if (ball.x <= getBallatEdgeCoord(BALL_RADIUS, 0)) {
                 contact.left = true;
             }
-            if( ball.x >= getBallatEdgeCoord(BALL_RADIUS,canvas.width)){
+            if (ball.x >= getBallatEdgeCoord(BALL_RADIUS, canvas.width)) {
                 contact.right = true;
+            }
+
+            if(ball.y <= tri.ymax && ball.x <= tri.xmax && ball.x >= tri.xmin ) {
+                contact.bottom = true;
+            }else if(ball.y >= tri.ymin && ball.x <= tri.xmax && ball.x >= tri.xmin ){
+                contact.top = true;
+            }else if(ball.x <= tri.xmax && ball.y <= tri.ymax && ball.y >= tri.ymin ){
+                contact.right = true;
+            }else if(ball.x >= tri.xmin && ball.y <= tri.ymax && ball.y >= tri.ymin ){
+                contact.left = true;
             }
             return contact;
         };
@@ -156,16 +164,47 @@ angular.module('RicochetApp')
             this.radius = attrs.radius;
             this.name = attrs.name;
             this.vx = attrs.vx;
-            this.vy = attrs.vx;
+            this.vy = attrs.vy;
             this.dx = attrs.dx;
             this.dy = attrs.dy;
             this.context = attrs.context;
-            this.draw = $scope.drawFunc;
             this.target = {x:0,y:0};
-            this.drawit = function(){
-                this.draw(this.context);
+            this.drawit = function() {
+                // Here, we'll first begin drawing the path and then use the arc() function to draw the circle. The arc function accepts 6 parameters, x position, y position, radius, start angle, end angle and a boolean for anti-clockwise direction.
+                this.context.beginPath();
+                this.context.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+                //createRadialGradient(x0, y0, r0, x1, y1, r1);
+                var radialGradient = this.context.createRadialGradient(this.x+2, this.y+2, BALL_RADIUS, this.x+2, this.y+2, 1);
+                radialGradient.addColorStop(1,'#fff' );
+                radialGradient.addColorStop(0, this.color);
+                this.context.fillStyle = radialGradient;
+                this.context.fill();
+                this.context.closePath();
             };
-            this.drawit();
+            this.drawit(this.context);
         };
+
+        var Tri = function(attrs){
+            this.context = attrs.context;
+            this.vert1   = [75, 50];  //attrs.vert1;
+            this.vert2   = [100, 75]; //attrs.vert2;
+            this.vert3   = [100, 25]; //attrs.vert3;
+            this.drawit  = function() {
+                $scope.context.beginPath();
+                $scope.context.moveTo(this.vert1[0],this.vert1[1]);
+                $scope.context.lineTo(this.vert2[0],this.vert2[1]);
+                $scope.context.lineTo(this.vert3[0],this.vert3[1]);
+                $scope.context.fill();
+            };
+            this.init = function(){
+                this.xmax = _.max( [ this.vert1[0], this.vert2[0], this.vert3[0] ] );
+                this.xmin = _.min( [ this.vert1[0], this.vert2[0], this.vert3[0] ] );
+                this.ymax = _.max( [ this.vert1[1], this.vert2[1], this.vert3[1] ] );
+                this.ymin = _.min( [ this.vert1[1], this.vert2[1], this.vert3[1] ] );
+                this.drawit();
+            };
+            this.init();
+        };
+
         $scope.init();
 });
